5242967132c13d1c25697b26b21c45df3d1ec2e0
Luis-Lizarme <i201521389@cibertec.edu.pe>
2024-08-18 23:53:57 +0000

    primer avance

---
+++ T2/src/componentes/Menu.jsx
@@ -0,0 +1,12 @@
+function Menu() {
+return <div>
+                <li><a href="#">Quiénes somos</a></li>
+                <li><a href="#">Dónde viajar</a></li>
+                <li><a href="#">Dónde quedarse</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contacto</a></li>
+    
+</div>
+}
+
+export default Menu;
