20c5bbf79f7aa7ecd80abdb8ff1ea46f145e4117
Luis-Lizarme <i201521389@cibertec.edu.pe>
2024-08-19 02:50:00 +0000

    Final 1

--- T2/src/App.jsx
+++ T2/src/App.jsx
@@ -1,27 +1,22 @@
 import { Fragment } from 'react';
-import Menu from './componentes/Menu'
-import Carrusel from './componentes/Carrusel'
+import Menu from './componentes/Menu';
+import Carrusel from './componentes/Carrusel';
 import Conoce from './componentes/Conoce';
 import Reloj from './componentes/Reloj';
-import Galeria  from './componentes/Galeria';
+import Galeria from './componentes/Galeria';
+import Pie from './componentes/Pie';
 
-
-function App(){
+function App() {
   return (
     <>
-    <Menu /> 
-     
-    <Carrusel />
-    
-    <Conoce />
-
-    <Reloj />
-    
-    <Galeria />
-
-
+      <Menu />
+      <Carrusel />
+      <Conoce />
+      <Reloj />
+      <Galeria />
+      <Pie />
     </>
-  )
+  );
 }
 
 export default App;
---
+++ T2/src/componentes/Pie.jsx
@@ -0,0 +1,14 @@
+import './Estilo.css';
+import { Fragment } from 'react';
+function Pie(){
+    return(
+<div class="footer">
+    <a href="https://www.peru.travel/es" target="_blank">Perú Travel</a>
+    <a href="https://meetings.peru.travel/es/blog" target="_blank">Promperú Blog</a>
+    <a href="https://peru.info/es-pe/" target="_blank">Info Perú</a>
+    <a href="https://www.aa.com/homePage.do?locale=es_AR" target="_blank">Llama Airlines</a>
+</div>
+    )
+}
+
+export default Pie;
