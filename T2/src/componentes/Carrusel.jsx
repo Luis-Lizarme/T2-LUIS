c51f71548a5a2b1a282dd657d168cd0c9ae0017c
Luis-Lizarme <i201521389@cibertec.edu.pe>
2024-08-19 01:59:17 +0000

    avance3

--- T2/src/componentes/Carrusel.jsx
+++ T2/src/componentes/Carrusel.jsx
@@ -1,9 +1,31 @@
-import { Fragment } from 'react';
-import './Estilo.css'
+import React, { useState, useEffect } from 'react';
+import './Estilo.css';
 
 function Carrusel() {
-    return (
-    <img src="src/componentes/IMAGENES/PORTADA.jpg" alt=""  className = 'banner'  />
-    ) }
+  const images = [
+    'src/componentes/IMAGENES/PORTADA.jpg',
+    'src/componentes/IMAGENES/BANNER2.jpg', // Replace with your image paths
+    'src/componentes/IMAGENES/BANNER3.jpg',
+  ];
 
-    export default Carrusel;
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000); // Change image every 3 seconds
+
+    return () => clearInterval(interval); // Cleanup on component unmount
+  }, [images.length]);
+
+  return (
+    <div className="carousel-container">
+      <img src={images[currentIndex]} alt="Carousel" className="banner" />
+    </div>
+  );
+}
+
+export default Carrusel;
+
